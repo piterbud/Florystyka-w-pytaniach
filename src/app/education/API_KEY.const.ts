4705642bf91ed36505d2f1e2dcd23a372dbeabb1
piterbud <piterbud@wp.pl>
2024-06-10 15:57:11 +0200

    html files formatting

--- src/app/education/API_KEY.const.ts
+++
@@ -1,1 +0,0 @@
-export const API_KEY = 'AIzaSyDm8n-CPKOHDAl38Vay7U8OWCyPuQ-N9-Q';
--- src/app/education/api.service.ts
+++ src/app/education/api.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { API_KEY } from './API_KEY.const';
 import { HttpClient } from '@angular/common/http';
 import { PlaylistItemsInterface } from './interfaces/playlist-items.interface';
 
@@ -10,7 +9,7 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getPlayListItems(playlistId: string) {
-    const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${API_KEY}&maxResults=12`;
+    const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=AIzaSyDm8n-CPKOHDAl38Vay7U8OWCyPuQ-N9-Q&maxResults=12`;
     return this.http.get<PlaylistItemsInterface>(url);
   }
 }
